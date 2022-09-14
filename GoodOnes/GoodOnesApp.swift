f734e3944caa66e13fe994282cd3f9722bbc6d0d
chavesyuri-mb <79917740+chavesyuri-mb@users.noreply.github.com>
2022-09-14 13:10:16 -0400

    Initial Commit

---
+++ GoodOnes/GoodOnesApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoodOnesApp.swift
+//  GoodOnes
+//
+//  Created by Yuri Chaves on 14/09/22.
+//
+
+import SwiftUI
+
+@main
+struct GoodOnesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
